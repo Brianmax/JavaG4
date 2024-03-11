71ed992b64fc1ba392a572b17d70c3953d5c7474
Brianmax <george.maxi@ucsp.edu.pe>
2024-03-11 15:46:55 -0500

    viernes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-import repaso.Dog;
+import ejercicio2.Computer;
+import herencia.Estudiante;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Dog myDog4 = new Dog();
+        Estudiante estudiante1 = new Estudiante(
+                "George",
+                "Maxi",
+                "gmaxi@gmail.com",
+                12.3);
+        estudiante1.
     }
 }
